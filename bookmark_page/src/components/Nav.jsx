0356b76ff4f3069326b1ff4a62275743e6c4cc61
Md Ahasan Habib Nahid <ahasanhnahid@gmail.com>
2024-08-21 08:50:17 +0600

    mobile nav complete

--- bookmark_page/src/components/Nav.jsx
+++ bookmark_page/src/components/Nav.jsx
@@ -1,38 +1,54 @@
-import logo_light from '../assets/images/logo-light.svg'
-import logo from '../assets/images/logo-bookmark.svg'
-import hamburger from '../assets/images/icon-hamburger.svg'
-import closeIcon from '../assets/images/icon-close.svg'
-import facebook from '../assets/images/icon-facebook.svg'
-import twiteer from '../assets/images/icon-twitter.svg'
+import logo_light from '../assets/images/logo-light.svg';
+import logo from '../assets/images/logo-bookmark.svg';
+import hamburger from '../assets/images/icon-hamburger.svg';
+import closeIcon from '../assets/images/icon-close.svg';
+import facebook from '../assets/images/icon-facebook.svg';
+import twitter from '../assets/images/icon-twitter.svg';
+import { useState } from 'react';
 
 function Nav() {
+
+  const [openMobNav, setOpenMobNav] = useState(false);
+
+  const handleNav = () => {
+    setOpenMobNav(prevState => !prevState);
+  }
+
   return (
     <div>
-       <header className='flex items-center justify-between p-8'>
-      <div>
-        <img src={logo} alt="" />
-      </div>
+      <header className='flex items-center justify-between p-10'>
+        <div>
+          <img src={logo} alt="Logo" />
+        </div>
 
-        <nav className='hidden'>
-          <ul>
-            <li><a href="">Feature</a></li>
-            <li><a href="">Pricing</a></li>
-            <li><a href="">Contact</a></li>
-            <li><a href="">login</a></li>
-          </ul>
+        <nav className={`${openMobNav ? 'open' : 'md:block'} hidden`}>
+          <div className='nav__menu'>
+            <div className='flex items-center justify-between md:hidden'>
+              <img src={logo_light} alt="Light Logo" />
+              <button onClick={handleNav}>
+                <img src={closeIcon} alt="Close Menu" />
+              </button>
+            </div>
+            <ul className='nav__list md:flex items-center justify-center md:gap-6 uppercase'>
+              <li className='hover:text-soft-red'><a href="#">Feature</a></li>
+              <li className='hover:text-soft-red'><a href="#">Pricing</a></li>
+              <li className='hover:text-soft-red'><a href="#">Contact</a></li>
+              <li><a className='nav__btn' href="#">Login</a></li>
+            </ul>
+          </div>
 
-          <ul>
-            <li><a href="/facebook"><img src={facebook} alt="facebook" /></a></li>
-            <li><a href="/twiteer"><img src={twiteer} alt="twiteer" /></a></li>
+          <ul className='nav_link md:hidden'>
+            <li><a href="/facebook"><img src={facebook} alt="Facebook" /></a></li>
+            <li><a href="/twitter"><img src={twitter} alt="Twitter" /></a></li>
           </ul>
         </nav>
 
-        <button>
-          <img src={hamburger} alt="menu" />
+        <button className='md:hidden' onClick={handleNav}>
+          <img src={hamburger} alt="Menu" />
         </button>
       </header>
     </div>
   )
 }
 
-export default Nav
+export default Nav;
